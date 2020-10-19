7aae4e5befacee37da0ac6159a1de618c2f6ef0d
Siddhant Kushwaha <30691250+siddhantkushwaha@users.noreply.github.com>
2020-10-19 22:15:51 +0530

    Create bubble_sort.cpp

---
+++ Sorting/bubble_sort.cpp
@@ -0,0 +1,39 @@
+/*
+    Bubble Sort
+    a. Sorting algorithm
+    b. Comparsion based
+    c. Time - O(n^2)
+    d. Space - O(n)
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define printarr          \
+    for (auto a : arr)    \
+        cout << a << ' '; \
+    cout << '\n';
+
+void sort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1, 4, -1, 0, 23, 12, 78};
+    sort(arr);
+
+    printarr(arr);
+
+    return 0;
+}
