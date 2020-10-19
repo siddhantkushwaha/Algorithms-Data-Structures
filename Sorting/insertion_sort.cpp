9d9467c2f6d94195c1a45dc3ed3bda7bb0d8997f
Siddhant Kushwaha <30691250+siddhantkushwaha@users.noreply.github.com>
2020-10-19 22:03:26 +0530

    Create insertion_sort.cpp

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define printarr for (auto a : arr) cout << a << ' '; cout << '\n';
+
+void sort(vector<int> &arr) {
+    int n = arr.size();
+
+    // If key's at position i, we're assuming prefix array is already sorted
+    for (int i = 0; i < n; i++) {
+
+        int j = i, key = arr[i];
+
+        // If current key is smaller than the previous element, keeping moving it deeper in the prefix array
+        while (j-1 >= 0 && key < arr[j-1]) {
+            arr[j] = arr[j-1];
+            j--;
+        }
+        arr[j] = key;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1, 4, -1, 0, 23, 12, 78};
+    sort(arr);
+
+    printarr(arr);
+
+    return 0;
+}
